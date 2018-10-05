a1d0c813392f9bd747ad707108b22411b56afb61
GabrielRubio <GabrielRubio>
2018-10-04 23:52:13 -0300

    ajustando email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,11 +16,11 @@ $assunto = strip_tags(htmlspecialchars($_POST['assunto']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'gabriel.rubio.usp@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'contato@infobiojr.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_sender = 'contato@infobiojr.com.br';
 $email_subject = "Site da EJ:  $assunto";
 $email_body = "Você recebeu um email do site da EJ via formulario de contato.\n\n"."Aqui estao os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nAssunto: $assunto\n\nMensage:\n$message";
-$headers = "From: gabriel.rubio.usp@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: contato@infobiojr.com.br'\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers, "-r".$to);
 return true;         
