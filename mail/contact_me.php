83c4967b2bfb357cae3bc993339b1fda163b86e9
Fellimao <40434106+fellimao@users.noreply.github.com>
2020-05-11 19:11:26 -0300

    Adicionado tradução libras, novos membros e novas imagens

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -24,15 +24,15 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 
 $mail = new PHPMailer;
 
-$email_da_infobio = 'fellimao.almeida@gmail.com';
+$email_da_infobio = 'infobiojunior@gmail.com';
    
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com;smtp2.example.com';     // Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com;smtp2.gmail.com';     // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = $email_da_infobio;                  // SMTP username
-$mail->Password = '&SaiDaqui017$';                    // SMTP password
+$mail->Password = 'InfobioJr@0!8';                    // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
